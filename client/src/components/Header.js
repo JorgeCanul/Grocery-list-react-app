915a9b07a62d6769182ee9c2d2a323431f4ce24b
KristaTollefson <chivas818@gmail.com>
2022-01-16 08:04:39 -0800

    add css background img, colors

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,7 +9,7 @@ export const Header = ({onForm, btnColor}) => {
            <h1>Grocery List</h1> 
            <Button onClick={onForm} 
            //if btnColor then btn color red add else blue
-           color={btnColor? 'red' : 'blue'} 
+           color={btnColor? 'red' : 'rgb(3, 114, 101)'} 
            //if btnColor then btn will display add else close
            text={btnColor? 'close form' : 'open form'}/>
         </header>
