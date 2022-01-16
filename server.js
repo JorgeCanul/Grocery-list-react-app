ab0affbc4ce67128cba2fdaddf357cb665d298ce
KristaTollefson <chivas818@gmail.com>
2022-01-15 21:36:09 -0800

    Prepare for deploy

--- server.js
+++ server.js
@@ -18,5 +18,14 @@ mongoose.connect(db)
 ///use routes
 app.use('/api/task', tasks);
 
-const PORT = 5000;
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html' ))
+    })
+}
+
+
+const PORT = process.env.PORT || 5000;
 app.listen(5000, () => console.log(`App runnig on ${PORT}`))
